cf1dff12028164d9e59ea2e91c095ae267598414
Valentin Vago <zeropaper@irata.ch>
2014-11-29 08:44:30 +0100

    impr(grunt): allow passing fetch options from grunt config
    Some of the options defined in the grnut target config are passed as
    request options.
    https://www.npmjs.org/package/request#request-options-callback-
    
    This commit also improves the handling of response status code.

--- src/tasks/fetch.js
+++ src/tasks/fetch.js
@@ -8,18 +8,72 @@ module.exports = function (grunt) {
     var done = this.async();
     var config = grunt.config('fetch.' + this.target);
 
+    config.options = config.options || {};
+
+    // https://www.npmjs.org/package/request#request-options-callback-
+    var toCopy = [
+      'uri',
+      'url',
+      'agentOptions',
+      'qs',
+      'useQuerystring',
+      'method',
+      'headers',
+      'body',
+      'form',
+      'formData',
+      'multipart',
+      'auth',
+      'json',
+      'preambleCRLF',
+      'postambleCRLF',
+      'followRedirect',
+      'followAllRedirects',
+      'maxRedirects',
+      'encoding',
+      'pool',
+      'timeout',
+      'proxy',
+      'oauth',
+      'hawk',
+      'strictSSL',
+      'agentOptions',
+      'jar',
+      'aws',
+      'httpSignature',
+      'localAddress',
+      'gzip',
+      'tunnel',
+      'proxyHeaderWhiteList',
+      'proxyHeaderExclusiveList'
+    ];
+
     function makeRequest(src, dest) {
       return function (cb) {
         grunt.log.writeln('Fetching ' + src);
-        request(src, function (error, response, body) {
-          if (!error && response.statusCode == 200) {
+
+        var requestOptions = {
+          url: src
+        };
+
+        for (var n in toCopy) {
+          var name = toCopy[n];
+          if (typeof config.options[name] !== 'undefined') {
+            requestOptions[name] = config.options[name];
+          }
+        }
+
+        request(requestOptions, function (error, response, body) {
+          if (!error && response.statusCode >= 200 && response.statusCode < 400) {
             grunt.verbose.writeln('fetched ' + src + ',');
             grunt.verbose.writeln('and writing to ' + dest + '');
             grunt.file.write(dest, body);
           }
           else {
-            grunt.log.error(error ? error : new Error('Status not 200'));
+            error = error || new Error('Status not OK (got ' + response.statusCode + ')');
+            grunt.log.error(error);
           }
+
           cb(error, dest);
         });
       };
